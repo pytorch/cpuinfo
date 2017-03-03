1747f8607a8c0fc408043b3046953cd439e4bcca
Marat Dukhan <maratek@gmail.com>
2017-03-03 05:00:48 -0500

    Use Mach host_info to get number of logical/physical processors

--- src/mach/topology.c
+++ src/mach/topology.c
@@ -4,38 +4,43 @@
 
 #include <sys/types.h>
 #include <sys/sysctl.h>
-#include <mach/api.h>
+#include <mach/host_info.h>
+#include <mach/mach_host.h>
 
 #include <log.h>
 #include <mach/api.h>
 
 
 struct cpuinfo_mach_topology cpuinfo_mach_detect_topology(void) {
+	host_basic_info_data_t basic_info;
+	mach_msg_type_number_t count = HOST_BASIC_INFO_COUNT;
+	kern_return_t host_info_result = host_info(mach_host_self(), HOST_BASIC_INFO, (host_info_t) &basic_info, &count);
+	if (host_info_result != KERN_SUCCESS) {
+		cpuinfo_log_error("host_info(HOST_BASIC_INFO) failed with error code %d", host_info_result);
+		return (struct cpuinfo_mach_topology) { .packages = 1, .cores = 1, .threads = 1 };
+	}
+	integer_t cores = basic_info.physical_cpu_max;
+	if (cores <= 0) {
+		cpuinfo_log_error("host_info(HOST_BASIC_INFO) returned invalid physical_cpu_max value %d", (int) cores);
+		cores = 1;
+	}
+	integer_t threads = basic_info.logical_cpu_max;
+	if (threads < cores) {
+		cpuinfo_log_error("host_info(HOST_BASIC_INFO) returned invalid logical_cpu_max value %d "
+			"physical_cpu_max = %d", (int) threads, (int) cores);
+		threads = cores;
+	}
+
 	int packages = 1;
 	size_t sizeof_packages = sizeof(packages);
 	if (sysctlbyname("hw.packages", &packages, &sizeof_packages, NULL, 0) != 0) {
 		cpuinfo_log_error("sysctlbyname(\"hw.packages\") failed: %s", strerror(errno));
 	} else if (packages <= 0) {
-		cpuinfo_log_error("invalid hw.packages value %d", packages);
+		cpuinfo_log_error("sysctlbyname(\"hw.packages\") returned invalid value %d", packages);
+		packages = 1;
 	}
 
-	int cores = 1;
-	size_t sizeof_cores = sizeof(cores);
-	if (sysctlbyname("hw.physicalcpu_max", &cores, &sizeof_cores, NULL, 0) != 0) {
-		cpuinfo_log_error("sysctlbyname(\"hw.physicalcpu_max\") failed: %s", strerror(errno));
-	} else if (cores <= 0) {
-		cpuinfo_log_error("invalid hw.physicalcpu_max value %d", cores);
-	}
-
-	int threads = cores;
-	size_t sizeof_threads = sizeof(threads);
-	if (sysctlbyname("hw.logicalcpu_max", &threads, &sizeof_threads, NULL, 0) != 0) {
-		cpuinfo_log_error("sysctlbyname(\"hw.logicalcpu_max\") failed: %s", strerror(errno));
-	} else if (threads < cores) {
-		cpuinfo_log_error("invalid hw.logicalcpu_max value %d (hw.physicalcpu_max = %d)", threads, cores);
-	}
-
-	cpuinfo_log_info("mach topology: packages = %d, cores = %d, threads = %d", packages, cores, threads);
+	cpuinfo_log_info("mach topology: packages = %d, cores = %d, threads = %d", packages, (int) cores, (int) threads);
 	struct cpuinfo_mach_topology topology = {
 		.packages = (uint32_t) packages,
 		.cores = (uint32_t) cores,
