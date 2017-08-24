6d7938e7c9ed85f1bc321e3ba53f9060e84859d3
Marat Dukhan <marat@fb.com>
2017-08-24 16:55:17 -0700

    Xiaomi Redmi 2A mock test

--- jni/Android.mk
+++ jni/Android.mk
@@ -201,6 +201,13 @@ LOCAL_STATIC_LIBRARIES := cpuinfo_mock gtest
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := xiaomi-redmi-2a-test
+LOCAL_SRC_FILES := $(LOCAL_PATH)/test/xiaomi-redmi-2a.cc
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/test
+LOCAL_STATIC_LIBRARIES := cpuinfo_mock gtest
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := meizu-pro-6s-test
 LOCAL_SRC_FILES := $(LOCAL_PATH)/test/meizu-pro-6s.cc
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/test
