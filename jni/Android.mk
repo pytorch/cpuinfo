6ce014a25de6ff29c4546ead7213d358cd3ebd18
Marat Dukhan <marat@fb.com>
2017-09-14 14:00:41 -0700

    Galaxy Tab 3 7.0 mock test

--- jni/Android.mk
+++ jni/Android.mk
@@ -184,6 +184,13 @@ LOCAL_STATIC_LIBRARIES := cpuinfo_mock gtest
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := galaxy-tab-3-7.0-test
+LOCAL_SRC_FILES := $(LOCAL_PATH)/test/galaxy-tab-3-7.0.cc
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/test
+LOCAL_STATIC_LIBRARIES := cpuinfo_mock gtest
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := galaxy-win-duos-test
 LOCAL_SRC_FILES := $(LOCAL_PATH)/test/galaxy-win-duos.cc
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/test
