b589f1f376e9559a023df8677352cf16602f950a
Marat Dukhan <marat@fb.com>
2017-10-19 12:02:00 -0700

    Huawei Mate 9 mock test

--- jni/Android.mk
+++ jni/Android.mk
@@ -428,6 +428,13 @@ LOCAL_STATIC_LIBRARIES := cpuinfo_mock gtest
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := huawei-mate-9-test
+LOCAL_SRC_FILES := $(LOCAL_PATH)/test/mock/huawei-mate-9.cc
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/test/mock
+LOCAL_STATIC_LIBRARIES := cpuinfo_mock gtest
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := meizu-pro-6s-test
 LOCAL_SRC_FILES := $(LOCAL_PATH)/test/mock/meizu-pro-6s.cc
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/test/mock
