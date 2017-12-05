232ef17debf40e8c8bc73876aaeaa50a76b4c4f0
Marat Dukhan <marat@fb.com>
2017-12-04 19:04:59 -0800

    Fix script to run x86 mock test on Android

--- scripts/android-x86-mock.sh
+++ scripts/android-x86-mock.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-adb push libs/armeabi-v7a/memo-pad-7-test /data/local/tmp/memo-pad-7-test
-adb push libs/armeabi-v7a/zenfone-2-test /data/local/tmp/zenfone-2-test
-adb push libs/armeabi-v7a/zenfone-2e-test /data/local/tmp/zenfone-2e-test
-adb push libs/armeabi-v7a/zenfone-c-test /data/local/tmp/zenfone-c-test
+adb push libs/x86/memo-pad-7-test /data/local/tmp/memo-pad-7-test
+adb push libs/x86/zenfone-2-test /data/local/tmp/zenfone-2-test
+adb push libs/x86/zenfone-2e-test /data/local/tmp/zenfone-2e-test
+adb push libs/x86/zenfone-c-test /data/local/tmp/zenfone-c-test
 
 adb shell "/data/local/tmp/memo-pad-7-test --gtest_color=yes"
 adb shell "/data/local/tmp/zenfone-2-test --gtest_color=yes"
