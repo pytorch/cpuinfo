a9e55a6afde89722af7ee30dc7bd27435e96226b
Marat Dukhan <maratek@gmail.com>
2018-02-20 01:31:23 -0500

    Fix uninitialized memory bug on macOS

--- src/x86/mach/init.c
+++ src/x86/mach/init.c
@@ -48,6 +48,7 @@ void cpuinfo_x86_mach_init(void) {
 	}
 
 	struct cpuinfo_x86_processor x86_processor;
+	memset(&x86_processor, 0, sizeof(x86_processor));
 	cpuinfo_x86_init_processor(&x86_processor);
 
 	const uint32_t threads_per_core = mach_topology.threads / mach_topology.cores;
