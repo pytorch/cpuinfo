784f94346eb5caefec58766129503c58bf89f4e4
Marat Dukhan <marat@fb.com>
2018-03-08 10:00:25 -0800

    Update Android test scripts to run chipset name tests

--- scripts/android-arm64-test.sh
+++ scripts/android-arm64-test.sh
@@ -4,5 +4,7 @@ set -e
 
 adb push libs/arm64-v8a/init-test /data/local/tmp/init-test
 adb push libs/arm64-v8a/cache-test /data/local/tmp/cache-test
+adb push libs/arm64-v8a/chipset-test /data/local/tmp/chipset-test
 adb shell /data/local/tmp/init-test --gtest_color=yes
 adb shell /data/local/tmp/cache-test --gtest_color=yes
+adb shell /data/local/tmp/chipset-test --gtest_color=yes
