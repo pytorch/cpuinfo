ab703bc15dcfb9e2765cce75d7d207e261da4076
Norah <norah.sakal@gmail.com>
2018-12-14 01:41:39 -0800

    Fixed possible misspelling in error message
    I think this might be a misspelling; a small fix, however, would make it easier to find additional person experiencing the same error.

--- src/linux/processors.c
+++ src/linux/processors.c
@@ -227,7 +227,7 @@ static bool max_processor_number_parser(uint32_t processor_list_start, uint32_t 
 uint32_t cpuinfo_linux_get_max_possible_processor(uint32_t max_processors_count) {
 	uint32_t max_possible_processor = 0;
 	if (!cpuinfo_linux_parse_cpulist(POSSIBLE_CPULIST_FILENAME, max_processor_number_parser, &max_possible_processor)) {
-		cpuinfo_log_error("failed to parse the list of possible procesors in %s", POSSIBLE_CPULIST_FILENAME);
+		cpuinfo_log_error("failed to parse the list of possible processors in %s", POSSIBLE_CPULIST_FILENAME);
 		return max_processors_count;
 	}
 	if (max_possible_processor >= max_processors_count) {
@@ -242,7 +242,7 @@ uint32_t cpuinfo_linux_get_max_possible_processor(uint32_t max_processors_count)
 uint32_t cpuinfo_linux_get_max_present_processor(uint32_t max_processors_count) {
 	uint32_t max_present_processor = 0;
 	if (!cpuinfo_linux_parse_cpulist(PRESENT_CPULIST_FILENAME, max_processor_number_parser, &max_present_processor)) {
-		cpuinfo_log_error("failed to parse the list of present procesors in %s", PRESENT_CPULIST_FILENAME);
+		cpuinfo_log_error("failed to parse the list of present processors in %s", PRESENT_CPULIST_FILENAME);
 		return max_processors_count;
 	}
 	if (max_present_processor >= max_processors_count) {
@@ -288,7 +288,7 @@ bool cpuinfo_linux_detect_possible_processors(uint32_t max_processors_count,
 	if (cpuinfo_linux_parse_cpulist(POSSIBLE_CPULIST_FILENAME, detect_processor_parser, &context)) {
 		return true;
 	} else {
-		cpuinfo_log_warning("failed to parse the list of possible procesors in %s", POSSIBLE_CPULIST_FILENAME);
+		cpuinfo_log_warning("failed to parse the list of possible processors in %s", POSSIBLE_CPULIST_FILENAME);
 		return false;
 	}
 }
@@ -305,7 +305,7 @@ bool cpuinfo_linux_detect_present_processors(uint32_t max_processors_count,
 	if (cpuinfo_linux_parse_cpulist(PRESENT_CPULIST_FILENAME, detect_processor_parser, &context)) {
 		return true;
 	} else {
-		cpuinfo_log_warning("failed to parse the list of present procesors in %s", PRESENT_CPULIST_FILENAME);
+		cpuinfo_log_warning("failed to parse the list of present processors in %s", PRESENT_CPULIST_FILENAME);
 		return false;
 	}
 }
