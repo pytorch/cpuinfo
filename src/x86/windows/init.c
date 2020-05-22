ba2ca40c0534d355a99564e60e3e69d450f4e5b0
Syoyo Fujita <syoyo@lighttransport.com>
2020-05-22 13:14:38 +0900

    Using lower-case Win32 header filename should be always safe.

--- src/x86/windows/init.c
+++ src/x86/windows/init.c
@@ -8,12 +8,7 @@
 #include <cpuinfo/internal-api.h>
 #include <cpuinfo/log.h>
 
-#if defined(__MINGW32__)
-// Windows header filename must be all lower case on MinGW
 #include <windows.h>
-#else
-#include <Windows.h>
-#endif
 
 #ifdef __GNUC__
   #define CPUINFO_ALLOCA __builtin_alloca
