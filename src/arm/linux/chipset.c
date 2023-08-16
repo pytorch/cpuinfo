pytorch/cpuinfo

commit baa1ee9df218a9adf568f39587c4339e1adf81a8

Fix UB in load_u16le/load_u24le/load_u32le

src/arm/linux/chipset.c
@@ -37,29 +37,18 @@ static inline bool is_ascii_numeric(char c) {
 }
 
 static inline uint16_t load_u16le(const void* ptr) {
-#if defined(__ARM_ARCH_7A__) || defined(__aarch64__)
-    return *((const uint16_t*) ptr);
-#else
 	const uint8_t* byte_ptr = (const uint8_t*) ptr;
 	return ((uint16_t) byte_ptr[1] << 8) | (uint16_t) byte_ptr[0];
-#endif
 }
 
 static inline uint32_t load_u24le(const void* ptr) {
-#if defined(__ARM_ARCH_7A__) || defined(__aarch64__)
-    return ((uint32_t) ((const uint8_t*) ptr)[2] << 16) | ((uint32_t) *((const uint16_t*) ptr));
-#else
 	const uint8_t* byte_ptr = (const uint8_t*) ptr;
-	return ((uint32_t) byte_ptr[2] << 16) | ((uint32_t) byte_ptr[1] << 8) | (uint32_t) byte_ptr[0];
-#endif
+	return ((uint32_t) byte_ptr[2] << 16) | (uint32_t) load_u16le(byte_ptr + 1);
 }
 
 static inline uint32_t load_u32le(const void* ptr) {
-#if defined(__ARM_ARCH_7A__) || defined(__aarch64__)
-    return *((const uint32_t*) ptr);
-#else
-	return ((uint32_t) ((const uint8_t*) ptr)[3] << 24) | load_u24le(ptr);
-#endif
+	const uint8_t* byte_ptr = (const uint8_t*) ptr;
+	return ((uint32_t) byte_ptr[3] << 24) | ((uint32_t) byte_ptr[2] << 16) | ((uint32_t) byte_ptr[1] << 8) | (uint32_t) byte_ptr[0];
 }
 
 /*

commit 1f6474637394de7c18a92daff1a260b79c6f35f0

Fix a bug in load_u24le introduced in #178

src/arm/linux/chipset.c
@@ -42,8 +42,7 @@ static inline uint16_t load_u16le(const void* ptr) {
 }
 
 static inline uint32_t load_u24le(const void* ptr) {
-	const uint8_t* byte_ptr = (const uint8_t*) ptr;
-	return ((uint32_t) byte_ptr[2] << 16) | (uint32_t) load_u16le(byte_ptr + 1);
+	return ((uint32_t) ((const uint8_t*) ptr)[2] << 16) | (uint32_t) load_u16le(ptr);
 }
 
 static inline uint32_t load_u32le(const void* ptr) {