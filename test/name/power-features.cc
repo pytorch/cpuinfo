8e598b5628f8c2038d23b00180c62562bc646d38
Manjul Mohan <manjul.mohan@ibm.com>
2024-05-07 02:03:49 -0500

    To add a custom testcase to test power features

--- test/name/power-features.cc
+++ test/name/power-features.cc
@@ -1,16 +1,27 @@
-#include <string.h>
 #include <cpuinfo.h>
 #include <gtest/gtest.h>
+#include <sys/auxv.h>
+
+extern "C" bool CPUINFO_ABI cpuinfo_initialize(void);
+extern "C" inline bool cpuinfo_has_powerpc_vsx(void);
+extern "C" inline bool cpuinfo_has_powerpc_htm(void);
+extern "C" inline bool cpuinfo_has_powerpc_mma(void);
 
 TEST(PowerFeatures, Power){
 	if (!cpuinfo_initialize()) {
 		fprintf(stderr, "failed to initialize CPU information\n");
 		exit(1);
 	}
-	EXPECT_EQ(1, cpuinfo_has_powerpc_htm());
-	EXPECT_EQ(1, cpuinfo_has_powerpc_mma());
-	EXPECT_EQ(1, cpuinfo_has_powerpc_vsx());
+	#if CPUINFO_MOCK
+		int a = mock_hwcap;
+	#else
+		int a = (uint32_t) getauxval(AT_HWCAP);
+		int b = (uint32_t) getauxval(AT_HWCAP2);
+	#endif
+
+	EXPECT_EQ(!!(b & UINT32_C(0x40000000)), cpuinfo_has_powerpc_htm());
+	EXPECT_EQ(!!(b & UINT32_C(0x00020000)), cpuinfo_has_powerpc_mma());
+	EXPECT_EQ(!!(a & UINT32_C(0x00000080)), cpuinfo_has_powerpc_vsx());
 
 	cpuinfo_deinitialize();
-
 }
