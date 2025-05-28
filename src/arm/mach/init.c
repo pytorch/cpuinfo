23759e8706ccbded03c4fc92da468f64477c26d5
Kimish Patel <kimishpatel@fb.com>
2025-05-28 09:50:23 -0700

    Update on "update apple soc info"
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    [ghstack-poisoned]

--- src/arm/mach/init.c
+++ src/arm/mach/init.c
@@ -146,7 +146,8 @@ static enum cpuinfo_uarch decode_uarch(uint32_t cpu_family, uint32_t core_index,
 
 		case CPUFAMILY_ARM_TAHITI:
 			/* Hexa-core: 2x Avalanche + 4x Blizzard */
-			return core_index + 4 < core_count ? cpuinfo_uarch_tahiti_everest : cpuinfo_uarch_tahiti_sawtooth;
+			return core_index + 4 < core_count ? cpuinfo_uarch_tahiti_everest
+							   : cpuinfo_uarch_tahiti_sawtooth;
 
 		default:
 			/* Use hw.cpusubtype for detection */
