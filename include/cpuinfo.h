a97a7d2db111a2ec77402ca440d35485bfc3702c
Frank Barchard <fbarchard@google.com>
2025-05-29 19:36:23 -0700

    Add Intel Darkmont uarch
    - Darkmont is the uarch used in Clearwater Forest

--- include/cpuinfo.h
+++ include/cpuinfo.h
@@ -383,7 +383,7 @@ enum cpuinfo_uarch {
 	/** Intel Knights Mill Xeon Phi. */
 	cpuinfo_uarch_knights_mill = 0x00100504,
 	/** Intel Darkmont microarchitecture (e-core used in Clearwater Forest). */
-	cpuinfo_uarch_silvermont = 0x00100505,
+	cpuinfo_uarch_darkmont = 0x00100505,
 
 	/** Intel/Marvell XScale series. */
 	cpuinfo_uarch_xscale = 0x00100600,
