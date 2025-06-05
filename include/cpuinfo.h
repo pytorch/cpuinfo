f9f50aab341cd05b061e327b2a3539698d508461
Richard Winterton <rrwinterton@gmail.com>
2025-06-05 16:46:53 -0600

    Update cpuinfo.h
    updated comments to refer to intel microarchitecture per request to keep consistent.

--- include/cpuinfo.h
+++ include/cpuinfo.h
@@ -353,7 +353,7 @@ enum cpuinfo_uarch {
 	cpuinfo_uarch_palm_cove = 0x0010020B,
 	/** Intel Sunny Cove microarchitecture (10 nm, Ice Lake). */
 	cpuinfo_uarch_sunny_cove = 0x0010020C,
-	/** Intel Willow Cove (10 nm, Tiger Lake). */
+	/** Intel Willow Cove microarchitecture (10 nm, Tiger Lake). */
 	cpuinfo_uarch_willow_cove = 0x0010020D,
 
 	/** Pentium 4 with Willamette, Northwood, or Foster cores. */
