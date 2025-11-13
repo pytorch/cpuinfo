952a519883239e086e9a78299a6efb8f6614022f
Jimothy Branson <jimothy@bouncingsheep.org>
2025-11-13 15:46:12 -0800

    Allow for longer CPU names (#320)
    Increase CPUINFO_PACKAGE_NAME_MAX to allow for longer strings,
    such as "Snapdragon® X Elite - X1E78100 - Qualcomm® Oryon™ CPU",
    which is 53 characters and WideCharToMultiByte converts to a
    57 byte string.

--- include/cpuinfo.h
+++ include/cpuinfo.h
@@ -736,7 +736,7 @@ struct cpuinfo_cluster {
 	uint64_t frequency;
 };
 
-#define CPUINFO_PACKAGE_NAME_MAX 48
+#define CPUINFO_PACKAGE_NAME_MAX 64
 
 struct cpuinfo_package {
 	/** SoC or processor chip model name */
