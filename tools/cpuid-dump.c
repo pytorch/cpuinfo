7a23453a2e6f456978efe9c81477fa63956a6a24
Daniel Lenski <dlenski@gmail.com>
2025-11-20 14:39:48 -0800

    cpuid-dump should iterate through all CPUs
    As Intel puts it: "CPUID, by design, returns different values depending on
    the core it is executed on"
    (see https://www.intel.com/content/www/us/en/developer/articles/guide/12th-gen-intel-core-processor-gamedev-guide.html#inpage-nav-1-5-2:~:text=CPUID%2C%20by%20design%2C%20returns%20different%20values%20depending%20on%20the%20core%20it%20is%20executed%20on)
    
    In particular, leaves 1, 4, 0x0b, 0x1a, and 0x1f are known to vary by core.
    Leaf 0x1a differentiates core types on hybrid CPUs.
    
    In order to aid in exploration of CPUID contents, `cpuid-dump` should dump
    CPUID results from *all* CPUs, rather than just one.  This is currently
    implemented for Linux only, using the `sched_setaffinity(2)` system call.

--- tools/cpuid-dump.c
+++ tools/cpuid-dump.c
@@ -1,3 +1,9 @@
+#ifdef __linux__
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdlib.h>
+#endif
+
 #include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -63,119 +69,202 @@ static void print_cpuid_string(struct cpuid_regs regs, uint32_t eax, enum cpuid_
 	}
 }
 
+static void force_one_cpu(int cpu, int n_cpus) {
+	if (n_cpus > 1) {
+#ifdef __linux__
+		cpu_set_t mask_one;
+		CPU_ZERO(&mask_one);
+		CPU_SET(cpu, &mask_one);
+		if (sched_setaffinity(0, sizeof mask_one, &mask_one) != 0) {
+			char buf[80];
+			snprintf(buf, sizeof(buf), "sched_setaffinity to CPU %d failed", cpu);
+			perror(buf);
+			exit(1);
+		}
+#endif
+	}
+}
+
+static void print_cpu_index(int cpu, int n_cpus) {
+#ifdef __linux__
+	if (n_cpus > 1)
+		printf("cpu%.*d: ", (n_cpus < 10 ? 1 : (n_cpus < 100 ? 2 : 3)), cpu);
+#endif
+}
+
 int main(int argc, char** argv) {
 	const uint32_t max_base_index = cpuid(0).eax;
-	uint32_t max_structured_index = 0, max_trace_index = 0, max_socid_index = 0;
-	bool has_sgx = false;
-	for (uint32_t eax = 0; eax <= max_base_index; eax++) {
-		switch (eax) {
-			case UINT32_C(0x00000000):
-				print_cpuid_string(cpuid(eax), eax, REG_BDC);
-				break;
-			case UINT32_C(0x00000004):
-				for (uint32_t ecx = 0;; ecx++) {
-					const struct cpuid_regs regs = cpuidex(eax, ecx);
-					if ((regs.eax & UINT32_C(0x1F)) == 0) {
-						break;
-					}
-					print_cpuidex(regs, eax, ecx);
-				}
-				break;
-			case UINT32_C(0x00000007):
-				for (uint32_t ecx = 0; ecx <= max_structured_index; ecx++) {
-					const struct cpuid_regs regs = cpuidex(eax, ecx);
-					if (ecx == 0) {
-						max_structured_index = regs.eax;
-						has_sgx = !!(regs.ebx & UINT32_C(0x00000004));
-					}
-					print_cpuidex(regs, eax, ecx);
-				}
-				break;
-			case UINT32_C(0x0000000B):
-			case UINT32_C(0x0000001F): // Extended/V2
-				for (uint32_t ecx = 0;; ecx++) {
-					const struct cpuid_regs regs = cpuidex(eax, ecx);
-					if ((regs.ecx & UINT32_C(0x0000FF00)) == 0) {
-						break;
-					}
-					print_cpuidex(regs, eax, ecx);
-				}
-				break;
-			case UINT32_C(0x00000012):
-				if (has_sgx) {
+	uint32_t max_structured_index = 0, max_trace_index = 0, max_socid_index = 0, n_log_proc = 1;
+	bool has_sgx = false, is_hybrid = false;
+
+#ifdef __linux__
+	// TODO: handle case of >CPU_SETSIZE logical CPUs, or non-contiguity
+	cpu_set_t mask_default;
+	if (sched_getaffinity(0, sizeof mask_default, &mask_default) != 0) {
+		perror("sched_getaffinity failed");
+		exit(1);
+	}
+	n_log_proc = CPU_COUNT(&mask_default);
+#endif
+	if (max_base_index >= 1) {
+		// TODO: handle case of >256 logical CPUs, or multiple packages
+		const struct cpuid_regs regs = cpuid(1);
+		uint32_t n_apic_id;
+		if (regs.edx & UINT32_C(0x10000000)) { // Number of logical CPUs field is valid
+			n_apic_id = ((regs.ebx & UINT32_C(0x00ff0000)) >> 16);
+
+#ifdef __linux__
+			if (n_apic_id != n_log_proc)
+				fprintf(stderr,
+					"WARNING: %d logical CPUs per CPUID.01h.EBX[23:16] != %d per sched_getaffinity()\n",
+					n_apic_id,
+					n_log_proc);
+#else
+			if (n_apic_id > 1)
+				fprintf(stderr,
+					"WARNING: %d logical CPUs per CPUID.01h.EBX, results may vary by logical CPU\n",
+					n_apic_id);
+#endif
+		}
+	}
+
+	for (uint32_t lp = 0; lp < n_log_proc; lp++) {
+		force_one_cpu(lp, n_log_proc);
+
+		for (uint32_t eax = 0; eax <= max_base_index; eax++) {
+			switch (eax) {
+				case UINT32_C(0x00000000):
+					print_cpu_index(lp, n_log_proc);
+					print_cpuid_string(cpuid(eax), eax, REG_BDC);
+					break;
+				case UINT32_C(0x00000004):
 					for (uint32_t ecx = 0;; ecx++) {
 						const struct cpuid_regs regs = cpuidex(eax, ecx);
-						if (ecx >= 2 && (regs.eax & UINT32_C(0x0000000F)) == 0) {
+						if ((regs.eax & UINT32_C(0x1F)) == 0) {
 							break;
 						}
+						print_cpu_index(lp, n_log_proc);
 						print_cpuidex(regs, eax, ecx);
 					}
-				}
-				break;
-			case UINT32_C(0x00000014):
-				for (uint32_t ecx = 0; ecx <= max_trace_index; ecx++) {
-					const struct cpuid_regs regs = cpuidex(eax, ecx);
-					if (ecx == 0) {
-						max_trace_index = regs.eax;
+					break;
+				case UINT32_C(0x00000007):
+					for (uint32_t ecx = 0; ecx <= max_structured_index; ecx++) {
+						const struct cpuid_regs regs = cpuidex(eax, ecx);
+						if (ecx == 0) {
+							max_structured_index = regs.eax;
+							has_sgx = !!(regs.ebx & UINT32_C(0x00000004));
+						}
+						print_cpu_index(lp, n_log_proc);
+						print_cpuidex(regs, eax, ecx);
 					}
-					print_cpuidex(regs, eax, ecx);
-				}
-				break;
-			case UINT32_C(0x00000017):
-				for (uint32_t ecx = 0; ecx <= max_socid_index; ecx++) {
-					const struct cpuid_regs regs = cpuidex(eax, ecx);
-					if (ecx == 0) {
-						max_socid_index = regs.eax;
+					break;
+				case UINT32_C(0x0000000B):
+				case UINT32_C(0x0000001F): // Extended/V2
+					for (uint32_t ecx = 0;; ecx++) {
+						const struct cpuid_regs regs = cpuidex(eax, ecx);
+						if ((regs.ecx & UINT32_C(0x0000FF00)) == 0) {
+							break;
+						}
+						print_cpu_index(lp, n_log_proc);
+						print_cpuidex(regs, eax, ecx);
 					}
-					print_cpuidex(regs, eax, ecx);
-				}
-				break;
-			case UINT32_C(0x00000024):
-				for (uint32_t ecx = 0; ecx <= max_socid_index; ecx++) {
-					const struct cpuid_regs regs = cpuidex(eax, ecx);
-					if (ecx == 0) {
-						max_socid_index = regs.eax;
+					break;
+				case UINT32_C(0x00000012):
+					if (has_sgx) {
+						for (uint32_t ecx = 0;; ecx++) {
+							const struct cpuid_regs regs = cpuidex(eax, ecx);
+							if (ecx >= 2 && (regs.eax & UINT32_C(0x0000000F)) == 0) {
+								break;
+							}
+							print_cpu_index(lp, n_log_proc);
+							print_cpuidex(regs, eax, ecx);
+						}
 					}
-					print_cpuidex(regs, eax, ecx);
-				}
-				break;
-			default:
-				print_cpuid(cpuidex(eax, 0), eax);
-				break;
+					break;
+				case UINT32_C(0x00000014):
+					for (uint32_t ecx = 0; ecx <= max_trace_index; ecx++) {
+						const struct cpuid_regs regs = cpuidex(eax, ecx);
+						if (ecx == 0) {
+							max_trace_index = regs.eax;
+						}
+						print_cpu_index(lp, n_log_proc);
+						print_cpuidex(regs, eax, ecx);
+					}
+					break;
+				case UINT32_C(0x00000017):
+					for (uint32_t ecx = 0; ecx <= max_socid_index; ecx++) {
+						const struct cpuid_regs regs = cpuidex(eax, ecx);
+						if (ecx == 0) {
+							max_socid_index = regs.eax;
+						}
+						print_cpu_index(lp, n_log_proc);
+						print_cpuidex(regs, eax, ecx);
+					}
+					break;
+				case UINT32_C(0x00000024):
+					for (uint32_t ecx = 0; ecx <= max_socid_index; ecx++) {
+						const struct cpuid_regs regs = cpuidex(eax, ecx);
+						if (ecx == 0) {
+							max_socid_index = regs.eax;
+						}
+						print_cpu_index(lp, n_log_proc);
+						print_cpuidex(regs, eax, ecx);
+					}
+					break;
+				default:
+					print_cpu_index(lp, n_log_proc);
+					print_cpuid(cpuidex(eax, 0), eax);
+					break;
+			}
+		}
+
+		/**
+		 * CPUID[1].ECX bit 31 is supposed to indicate whether or not
+		 * a hypervisor is running, but not all hypervisors set it.
+		 */
+		const uint32_t max_hypervisor_index = cpuid(UINT32_C(0x40000000)).eax;
+		for (uint32_t eax = UINT32_C(0x40000000); eax <= max_hypervisor_index; eax++) {
+			switch (eax) {
+				case UINT32_C(0x40000000):
+					print_cpu_index(lp, n_log_proc);
+					print_cpuid_string(cpuid(eax), eax, REG_BCD);
+					break;
+				case UINT32_C(0x40000001):
+					print_cpu_index(lp, n_log_proc);
+					print_cpuid_string(cpuid(eax), eax, REG_A);
+					break;
+				default:
+					print_cpu_index(lp, n_log_proc);
+					print_cpuid(cpuidex(eax, 0), eax);
+			}
+		}
+
+		const uint32_t max_extended_index = cpuid(UINT32_C(0x80000000)).eax;
+		for (uint32_t eax = UINT32_C(0x80000000); eax <= max_extended_index; eax++) {
+			switch (eax) {
+				case UINT32_C(0x80000000):
+					print_cpu_index(lp, n_log_proc);
+					print_cpuid_string(cpuid(eax), eax, REG_BDC);
+					break;
+				case UINT32_C(0x80000002):
+				case UINT32_C(0x80000003):
+				case UINT32_C(0x80000004):
+					print_cpu_index(lp, n_log_proc);
+					print_cpuid_string(cpuid(eax), eax, REG_ABCD);
+					break;
+				default:
+					print_cpu_index(lp, n_log_proc);
+					print_cpuid(cpuidex(eax, 0), eax);
+			}
 		}
 	}
 
-	/**
-	 * CPUID[1].ECX bit 31 is supposed to indicate whether or not
-	 * a hypervisor is running, but not all hypervisors set it.
-	 */
-	const uint32_t max_hypervisor_index = cpuid(UINT32_C(0x40000000)).eax;
-	for (uint32_t eax = UINT32_C(0x40000000); eax <= max_hypervisor_index; eax++) {
-		switch (eax) {
-			case UINT32_C(0x40000000):
-				print_cpuid_string(cpuid(eax), eax, REG_BCD);
-				break;
-			case UINT32_C(0x40000001):
-				print_cpuid_string(cpuid(eax), eax, REG_A);
-				break;
-			default:
-				print_cpuid(cpuidex(eax, 0), eax);
+	if (n_log_proc > 1) {
+#ifdef __linux__
+		if (sched_setaffinity(0, sizeof mask_default, &mask_default) != 0) {
+			perror("sched_setaffinity to restore process defaults failed");
+			exit(1);
 		}
-	}
-
-	const uint32_t max_extended_index = cpuid(UINT32_C(0x80000000)).eax;
-	for (uint32_t eax = UINT32_C(0x80000000); eax <= max_extended_index; eax++) {
-		switch (eax) {
-			case UINT32_C(0x80000000):
-				print_cpuid_string(cpuid(eax), eax, REG_BDC);
-				break;
-			case UINT32_C(0x80000002):
-			case UINT32_C(0x80000003):
-			case UINT32_C(0x80000004):
-				print_cpuid_string(cpuid(eax), eax, REG_ABCD);
-				break;
-			default:
-				print_cpuid(cpuidex(eax, 0), eax);
-		}
+#endif
 	}
 }
