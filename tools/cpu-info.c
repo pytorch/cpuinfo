bc3c01e230c6974283e4b89421cfb0e232435589
fbarchard <fbarchard@google.com>
2026-04-13 13:19:06 -0700

    Add Sapphire Rapids (Golden Cove) microarchitecture detection (#377)
    * Add Sapphire Rapids (Golden Cove) microarchitecture detection
    
    * Add Emerald Rapids and Granite Rapids microarchitecture detection

--- tools/cpu-info.c
+++ tools/cpu-info.c
@@ -82,6 +82,12 @@ static const char* uarch_to_string(enum cpuinfo_uarch uarch) {
 			return "Sunny Cove";
 		case cpuinfo_uarch_willow_cove:
 			return "Willow Cove";
+		case cpuinfo_uarch_golden_cove:
+			return "Golden Cove";
+		case cpuinfo_uarch_raptor_cove:
+			return "Raptor Cove";
+		case cpuinfo_uarch_redwood_cove:
+			return "Redwood Cove";
 		case cpuinfo_uarch_willamette:
 			return "Willamette";
 		case cpuinfo_uarch_prescott:
